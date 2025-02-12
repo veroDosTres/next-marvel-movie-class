4af5f31e1b0f7de4e58cf0fb04feadb62ad0fa4a
Vero <ramsa2389@gmail.com>
2025-02-12 10:19:44 +0100

    Primer commit

---
+++ api/classes/NextMovie.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+class NextMovie
+{
+  public function __construct(
+    private string $title,
+    private int $days_until,
+    private string $release_date,
+    private string $poster_url,
+    private string $following_production,
+    private string $overview
+  ) {}
+
+  public function get_until_message(): string
+  {
+    $days = $this->days_until;
+    return match (true) {
+      $days == 0 => "Hoy se estrena! 🎉",
+      $days == 1 => "Mañana se estrena! 🚀 ",
+      $days < 7  => "Se estrena esta semana 😱",
+      $days < 30 => "Se estrena este mes 📅 ",
+      default    => "$days hasta el estreno 📅"
+    };
+  }
+
+  public static function fetch_and_create_movie(string $api_url): NextMovie
+  {
+    $result = file_get_contents($api_url);
+    $data = json_decode($result, true);
+
+    return new self(
+      $data["title"],
+      $data["days_until"],
+      $data["release_date"],
+      $data["poster_url"],
+      $data["following_production"]["title"] ?? "Desconocido",
+      $data["overview"]
+    );
+  }
+
+  public function get_data()
+  {
+    return get_object_vars($this);
+  }
+}
---
+++ api/templates/styles.php
@@ -0,0 +1,20 @@
+<style>
+    :root {
+      color-scheme: light dark;
+    }
+
+    body {
+      display: grid;
+      place-content: center;
+    }
+
+    h1 {
+      text-align: center;
+    }
+
+    section {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  </style>
